448e216d72f996ddee7865b1fdfbc1ea52831517
Andrew Beverley <a.beverley@ctrlo.com>
2015-06-17 15:25:11 +0100

    Bump version and prep release

--- lib/Dancer2/Plugin/Auth/Extensible/Provider/DBIC.pm
+++ lib/Dancer2/Plugin/Auth/Extensible/Provider/DBIC.pm
@@ -6,7 +6,7 @@ use DBIx::Class::ResultClass::HashRefInflator;
 use Lingua::EN::Inflect::Phrase;
 use String::CamelCase qw(camelize);
 
-our $VERSION = '0.400';
+our $VERSION = '0.401';
 
 =head1 NAME 
 
